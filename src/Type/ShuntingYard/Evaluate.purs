91481ffc7861fb4906170a33635da0299e33581e
PureFunctor <purefunctor@gmail.com>
2021-05-06 08:04:35 +0800

    Create specialized token type

--- src/Type/ShuntingYard/Evaluate.purs
+++ src/Type/ShuntingYard/Evaluate.purs
@@ -15,3 +15,17 @@ class Evaluate e r | e -> r
 
 evaluate :: forall e r. Evaluate e r => Proxy e -> Proxy r
 evaluate _ = Proxy
+
+
+foreign import data Compose
+  :: forall a b c
+   . (b -> TypeExpr c)
+  -> (a -> TypeExpr b)
+  -> a
+  -> TypeExpr c
+
+instance evaluateCompose
+  :: ( Evaluate (g a) b
+     , Evaluate (f b) c
+     )
+  => Evaluate (Compose f g a) c
--- src/Type/ShuntingYard/Lexer.purs
+++ src/Type/ShuntingYard/Lexer.purs
@@ -1,10 +1,32 @@
 module Type.ShuntingYard.Lexer where
 
 import Type.Data.Boolean (class And)
-import Type.Data.List (type (:>), Cons', List', Nil')
+import Type.Data.List (type (:>), List', Nil')
+import Type.Data.Peano as Peano
 import Type.Proxy (Proxy(..))
-import Type.ShuntingYard.Evaluate (class Evaluate, TypeExpr, evaluate)
+import Type.ShuntingYard.Evaluate (class Evaluate, Compose, TypeExpr, evaluate)
 import Type.ShuntingYard.Prelude (class ChopT, class FilterT, class GroupByT, class MapT, IsNatural, Join, NotEquals)
+
+
+data TokenK
+
+foreign import data Operator :: Symbol -> TokenK
+foreign import data Natural :: Peano.Nat -> TokenK
+
+
+class ReadTokenT ( s :: Symbol ) ( t :: TokenK ) | s -> t
+
+instance readTokenTLP :: ReadTokenT "(" (Operator "(")
+else instance readTokenTRP :: ReadTokenT ")" (Operator ")")
+else instance readTokenTPlus :: ReadTokenT "+" (Operator "+")
+else instance readTokenTMinus :: ReadTokenT "-" (Operator "-")
+else instance readTokenTTimes :: ReadTokenT "*" (Operator "*")
+else instance readTokenTDivide :: ReadTokenT "/" (Operator "/")
+else instance readTokenTNatural :: (Peano.ParseNat x n) => ReadTokenT x (Natural n)
+
+foreign import data ReadToken :: Symbol -> TypeExpr TokenK
+
+instance readTokenEvaluate :: ReadTokenT t r => Evaluate (ReadToken t) r
 
 
 foreign import data GroupingPredicate :: Symbol -> Symbol -> TypeExpr Boolean
@@ -19,16 +41,23 @@ instance groupingPredicateEvaluate
 
 foreign import data Tokenize
   :: Symbol
-  -> TypeExpr (List' Symbol)
+  -> TypeExpr (List' TokenK)
 
 instance tokenizeEvaluate
   :: ( ChopT ws xs
      , GroupByT GroupingPredicate xs ys
      , FilterT (NotEquals (" " :> Nil')) ys zs
-     , MapT Join zs zs'
+     , MapT (Compose ReadToken Join) zs zs'
      )
   => Evaluate (Tokenize ws) zs'
 
 
-f :: Proxy (Cons' "(" (Cons' "1234" (Cons' "+" (Cons' "1234" (Cons' ")" Nil')))))
-f = evaluate (Proxy :: Proxy (Tokenize "(1234 + 1234)"))
+f :: Proxy
+  ( Operator "(" :>
+    Natural Peano.D50 :>
+    Operator "+" :>
+    Natural Peano.D50 :>
+    Operator ")" :>
+    Nil'
+  )
+f = evaluate (Proxy :: Proxy (Tokenize "(50 + 50)"))
